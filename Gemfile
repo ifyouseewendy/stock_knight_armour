b9332f596cb70ad20d980876c2d4c115056b16b0
wendi <ifyouseewendy@gmail.com>
2016-02-17 14:30:41 +0800

    Gemfile: Add celluloid

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra'
 gem 'mongoid'
+gem 'celluloid'
 gem 'stock_knight', path: '/Users/wendi/Workspace/stock_knight'
--- app.rb
+++ app.rb
@@ -1,12 +1,28 @@
 require 'sinatra'
-
+require 'celluloid/current'
 require 'mongoid'
 $:.unshift File.join(__FILE__, "../config")
 Mongoid.load!("config/mongoid.yml", :development)
 
-class User
+class Quote
   include Mongoid::Document
-  field :name, type: String
+  field :name,        type: String
+  field :symbol,      type: String
+  field :venue,       type: String
+  field :bid,         type: Integer
+  field :ask,         type: Integer
+  field :bidSize,     type: Integer
+  field :askSize,     type: Integer
+  field :bidDepth,    type: Integer
+  field :askDepth,    type: Integer
+  field :last,        type: Integer
+  field :lastSize,    type: Integer
+  field :lastTrade,   type: Time
+  field :quoteTime,   type: Time
+end
+
+class Manager
+  include Celluloid
 end
 
 get '/' do
