3db769d9ba3b227d05a6778c9dcccd487878e2a3
wendi <ifyouseewendy@gmail.com>
2016-02-17 22:12:03 +0800

    app: Add quote request placeholder

--- app.rb
+++ app.rb
@@ -6,3 +6,41 @@ require File.expand_path('../models/models', __FILE__)
 get '/' do
   erb :index
 end
+
+get '/quotes' do
+  {
+    xData: [1,2,3,4,5],
+    datasets: [
+      {
+        name: 'Last',
+        data: [10, 20, 50, 40, 30],
+        type: 'line',
+        unit: '',
+      },
+      {
+        name: 'Bid',
+        data: [10, 20, 50, 40, 30],
+        type: 'line',
+        unit: '',
+      },
+      {
+        name: 'BidDepth',
+        data: [10, 20, 50, 40, 30],
+        type: 'area',
+        unit: '',
+      },
+      {
+        name: 'Ask',
+        data: [10, 20, 50, 40, 30],
+        type: 'line',
+        unit: '',
+      },
+      {
+        name: 'AskDepth',
+        data: [10, 20, 50, 40, 30],
+        type: 'area',
+        unit: '',
+      },
+    ]
+  }.to_json
+end
