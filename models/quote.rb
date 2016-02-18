6a5b9a85d6e1e84871d64dd00eaba80658b54c9d
wendi <ifyouseewendy@gmail.com>
2016-02-18 10:21:03 +0800

    quote: Add default value

--- models/quote.rb
+++ models/quote.rb
@@ -3,14 +3,14 @@ class Quote
   field :name,        type: String
   field :symbol,      type: String
   field :venue,       type: String
-  field :bid,         type: Integer
-  field :ask,         type: Integer
-  field :bidSize,     type: Integer
-  field :askSize,     type: Integer
-  field :bidDepth,    type: Integer
-  field :askDepth,    type: Integer
-  field :last,        type: Integer
-  field :lastSize,    type: Integer
+  field :bid,         type: Integer, default: 0
+  field :ask,         type: Integer, default: 0
+  field :bidSize,     type: Integer, default: 0
+  field :askSize,     type: Integer, default: 0
+  field :bidDepth,    type: Integer, default: 0
+  field :askDepth,    type: Integer, default: 0
+  field :last,        type: Integer, default: 0
+  field :lastSize,    type: Integer, default: 0
   field :lastTrade,   type: String # Index by db.quotes.createIndex( { lastTrade: 1 }, { background: true } )
   field :quoteTime,   type: String
 end
