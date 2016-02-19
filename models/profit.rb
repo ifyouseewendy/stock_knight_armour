f3c8c0c869c97c0ae9e1d7153f85cdf5d7990fde
wendi <ifyouseewendy@gmail.com>
2016-02-19 19:52:49 +0800

    profit: Add model

---
+++ models/profit.rb
@@ -0,0 +1,15 @@
+require 'concurrent'
+
+class Profit
+  def initialize
+    @counter = Concurrent::AtomicFixnum.new(0)
+  end
+
+  def increment_by(val)
+    @counter.update{|obj| obj + val}
+  end
+
+  def value
+    @counter.value
+  end
+end
