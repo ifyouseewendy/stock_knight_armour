dae5aa07e89afcbce519146699d43491deacc1e8
wendi <ifyouseewendy@gmail.com>
2016-02-20 10:53:08 +0800

    quote: Combine last and avg compute buy_in_price

--- models/quote.rb
+++ models/quote.rb
@@ -13,4 +13,25 @@ class Quote
   field :lastSize,    type: Integer, default: 0
   field :lastTrade,   type: String # Index by db.quotes.createIndex( { lastTrade: 1 }, { background: true, unique: true } )
   field :quoteTime,   type: String
+
+  class << self
+    def buy_in_price
+      return 0 if Quote.count < 6
+
+      sample = Quote.order_by(lastTrade: :desc).limit(6)
+
+      last_bid = sample.first.bid
+
+      bids    = sample.pluck(:bid).reject(&:zero?).sort
+      range   = bids.count / 3
+      avg_bid = bids[range, range].avg
+
+      valid_rate = 0.05
+      if last_bid >= avg_bid*(1-valid_rate) && last_bid <= avg_bid*(1+valid_rate)
+        [last_bid, avg_bid].avg
+      else
+        avg_bid
+      end
+    end
+  end
 end
