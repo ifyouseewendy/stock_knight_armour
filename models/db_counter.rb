2f0ae48b0ccd3151eecdd9a35cbba8a67ad0141e
wendi <ifyouseewendy@gmail.com>
2016-02-21 23:08:17 +0800

    db_counter: Add model

---
+++ models/db_counter.rb
@@ -0,0 +1,26 @@
+class DbCounter
+  class MetaCounter
+    include Mongoid::Document
+    field :counter, type: Integer, default: 0
+    field :name,    type: String
+  end
+
+  attr_reader :criteria
+
+  def initialize(name)
+    id = MetaCounter.find_or_create_by(name: name).id
+    @criteria = MetaCounter.where(id: id)
+  end
+
+  def increment_by(val)
+    criteria.inc(counter: val)
+  end
+
+  def value
+    criteria.first.counter
+  end
+
+  def clear
+    MetaCounter.destroy_all
+  end
+end
