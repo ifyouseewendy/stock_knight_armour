cc43ec7330edd773c8dcbcc3505906682656f530
wendi <ifyouseewendy@gmail.com>
2016-02-27 17:12:01 +0800

    irrational_exuberance: Add solution

--- models/irrational_exuberance_dealer.rb
+++ models/irrational_exuberance_dealer.rb
@@ -1,34 +1,58 @@
 class IrrationalExuberanceDealer < Dealer
+  def buy_first_nail_price(to:)
+    price = to
+    amount, fill_price = buy(type: :limit, price: price, qty: Configuration::SHARE)
+    return false if amount.zero?
+
+    # base = amount * fill_price
+    # ask_price = price
+    #
+    # sell_block(type: :limit, price: ask_price, qty: amount, base: base)
+    return true
+  end
+
+  def sell_first_nail_price(to:)
+    price = to
+    amount, fill_price = sell(type: :limit, price: price, qty: Configuration::SHARE)
+    return false if amount.zero?
+
+    # base = amount * fill_price
+    # bid_price = price
+    #
+    # buy_block(type: :limit, price: bid_price, qty: amount, base: base)
+    return true
+  end
+
   def deal_buy_first
     puts "--> deal_buy_first"
-    return unless valid_share_value
+    # return unless valid_share_value
 
-    price = Quote.good_price(based_on: :ask, sample_count: 1)
+    price = good_price(based_on: :bid, sample_count: 1)
     return if price.zero?
 
-    bid_rate, bid_share = 1, (0.04/Configuration::DEALER)
-    bid_rate -= index * bid_share
+    bid_rate = 0.5
     bid = ( price * 100 * bid_rate ).to_i
 
+    puts "--> deal_buy_first: bid #{bid}"
     return if bid == 0
 
     amount, fill_price = buy(type: :limit, price: bid, qty: Configuration::SHARE)
+    puts "--> deal_buy_first: amount #{amount}"
     return 0 if amount.zero?
 
     base = amount * fill_price
-    ask_price = [fill_price+200, (bid*0.95).to_i].max
+    ask_price = fill_price + 200
 
     sell_block(type: :limit, price: ask_price, qty: amount, base: base)
   end
 
   def deal_sell_first
-    return unless valid_share_value
+    # return unless valid_share_value
 
-    price = Quote.good_price(based_on: :bid)
+    price = good_price(based_on: :bid, sample_count: 1)
     return if price.zero?
 
-    ask_rate, ask_share = 1, (0.04/Configuration::DEALER)
-    ask_rate += index * ask_share
+    ask_rate = 0.5
     ask = ( price * 100 * ask_rate ).to_i
 
     return if ask == 0
@@ -37,7 +61,7 @@ class IrrationalExuberanceDealer < Dealer
     return 0 if amount.zero?
 
     base = amount * fill_price
-    bid_price = [fill_price-200, (ask*1.05).to_i].min
+    bid_price = fill_price - 200
 
     buy_block(type: :limit, price: bid_price, qty: amount, base: base)
   end
